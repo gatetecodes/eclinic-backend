83ae4fb1d6393857693cab297c7f562e13d42720
Marcel Gatete <gatetecodes@gmail.com>
2025-10-28 15:45:26 +0200

    chore: change the seed command

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   schema: "./prisma/schema",
   migrations: {
     path: "./prisma/migrations",
-    seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
+    seed: "bun run prisma/seed.ts",
   },
 });
