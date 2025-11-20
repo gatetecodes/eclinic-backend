aceae8877a534d652a5a5eae6cda0330118add98
Marcel Gatete <gatetecodes@gmail.com>
2025-11-20 18:38:45 +0200

    feat: enhance inventory module

--- src/api/v1/approvals/approvals.controller.ts
+++ src/api/v1/approvals/approvals.controller.ts
@@ -156,7 +156,6 @@ export const getApprovalRequests = async (c: Context) => {
       httpCodes.OK as ContentfulStatusCode
     );
   } catch (error) {
-    
     return c.json(
       {
         error: error instanceof Error ? error.message : "Internal Server Error",
